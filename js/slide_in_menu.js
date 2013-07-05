2fd220bddffa6ca91b2161dcbfd0611b778fd9c6
flyveren <flyveren@gmail.com>
2013-07-05 16:28:45 +0200

    visibility toggle

--- js/slide_in_menu.js
+++ js/slide_in_menu.js
@@ -10,12 +10,12 @@ $( document ).ready(function() {
 	$('#page.hfeed.site').css('overflow','visible');
 
 //define button
-$(".wrapper").append("<div id='knap'></div>");
-$('#knap').css('width','45px');
-$('#knap').css('height','30px');
-$('#knap').css('margin-left','-40px');
-$('#knap').css('margin-top','-120px');
-$('#knap').css('background-color','red');
+	$(".wrapper").append("<div id='knap'></div>");
+	$('#knap').css('width','45px');
+	$('#knap').css('height','30px');
+	$('#knap').css('margin-left','-40px');
+	$('#knap').css('margin-top','-120px');
+	$('#knap').css('background-color','red');
 
 var $knapfunktion = 1;
 
@@ -25,11 +25,13 @@ $("#knap").click(function () {
 	$('#page').css('position','relative');
 	$('#page').css('left','0px');
 	$('#site-navigation.main-navigation').css('margin-left','0px');
+		$('#site-navigation.main-navigation').css('visibility','hidden');
 	$knapfunktion = 1;
 	}else{
 	$('#page').css('position','relative');
 	$('#page').css('left','150px');
 	$('#site-navigation.main-navigation').css('margin-left','-150px');
+	$('#site-navigation.main-navigation').css('visibility','visible');
 	$knapfunktion = 0;
 	}
 });
